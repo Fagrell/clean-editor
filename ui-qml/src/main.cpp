6832e6e2742d9855392b1a93359a2bc9505558f9
Alexander Fagrell <fagrell@outlook.com>
2018-11-11 23:09:57 +0000

    Added LineNumbers, DocumentHandler, DocumentsModel and fonts to main

--- ui-qml/src/main.cpp
+++ ui-qml/src/main.cpp
@@ -4,7 +4,10 @@
 #include <QFontDatabase>
 #include <QDebug>
 
-#include <main-controller.h>
+#include "main-controller.h"
+#include "document-handler.h"
+#include "documents-model.h"
+#include "line-numbers.h"
 
 static const constexpr auto kMainControllerInstance = "mainController";
 
@@ -16,17 +19,26 @@ int main(int argc, char *argv[])
 
   QGuiApplication app(argc, argv);
 
+  QStringList fonts = QStringList{} << ":/fonts/Hack-Regular.ttf" << ":/fonts/Hack-Bold.ttf"
+                                    << ":/fonts/Hack-BoldItalic.ttf" << ":/fonts/Hack-Italic.ttf"
+                                    << ":/fonts/fontello.ttf";
   QFontDatabase fontDatabase;
-  if (fontDatabase.addApplicationFont(":/fonts/fontello.ttf") == -1) {
-      qWarning() << "Failed to load fontello.ttf";
+  for (const auto& font : fonts) {
+    if (fontDatabase.addApplicationFont(font) == -1) {
+        qWarning() << "Failed to load font: " << font;
+    }
   }
 
   qmlRegisterType<CleanEditor::Controllers::MainController>("CleanEditor", 1, 0, kMainControllerInstance);
+  qmlRegisterType<CleanEditor::Logic::DocumentHandler>("CleanEditor", 1, 0, "DocumentHandler");
+  qmlRegisterType<CleanEditor::UI::LineNumbers>("CleanEditor", 1, 0, "LineNumbers");
 
   CleanEditor::Controllers::MainController mainController;
+  CleanEditor::Model::DocumentsModel documentsModel;
 
   QQmlApplicationEngine engine;
   engine.rootContext()->setContextProperty(kMainControllerInstance, &mainController);
+  engine.rootContext()->setContextProperty("documentsModel", &documentsModel);
   engine.load(QUrl(QStringLiteral("qrc:/views/MainView.qml")));
 
   if (engine.rootObjects().isEmpty()) {
