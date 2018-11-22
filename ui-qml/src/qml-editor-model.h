2099a99da4c7badcc167630c8b50a1b590f949ed
Alexander Fagrell <fagrell@outlook.com>
2018-11-22 22:01:49 +0000

    Added QmlEditorModel which is a EditorModel but extends it with a QQuickTextDocument so that it can be used together with a TextEdit in QML

---
+++ ui-qml/src/qml-editor-model.h
@@ -0,0 +1,34 @@
+#ifndef QML_EDITOR_MODEL_H
+#define QML_EDITOR_MODEL_H
+
+#include "editor-model.h"
+class QQuickTextDocument;
+
+namespace CleanEditor {
+namespace Models {
+
+class QmlEditorModel : public EditorModel {
+  Q_OBJECT
+  Q_DISABLE_COPY(QmlEditorModel)
+
+  Q_PROPERTY(QQuickTextDocument *document READ document WRITE setDocument NOTIFY documentChanged)
+
+public:
+  explicit QmlEditorModel(QObject *parent = nullptr);
+
+  QQuickTextDocument *document() const;
+  void setDocument(QQuickTextDocument *document);
+
+Q_SIGNALS:
+    void documentChanged();
+
+private:
+  QTextDocument* textDocument() const;
+
+  QQuickTextDocument* document_{nullptr};
+};
+
+} //namespace Models
+} //namespace CleanEditor
+
+#endif // QML_EDITOR_MODEL_H
